708374d8116d14724434829643b5a4c6280768fb
JINazxo筆記本 <43165736+axzoo@users.noreply.github.com>
2019-06-12 11:12:19 +0800

    Add some note for set.

---
+++ New Unity Project class03/Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class GameManager :MonoBehaviour
+{
+    public Ship ship01 = new Ship();
+
+    public Ship ship01_kind = new Ship(84.5f, 44.2f, 76.1f);
+
+    private void Start ()
+    {
+        ship01.speed = 20.6f;
+        Debug.Log("ship speed :" + ship01.speed);
+
+        ship01.length = 74.8f;
+        Debug.Log("ship length :" + ship01.length);
+
+        //ship01.width = 12.4f;
+        Debug.Log("ship width :" + ship01.Width);
+
+        Debug.Log("ship large :" + ship01.volume);
+
+        Debug.Log("ship large kind :" + ship01_kind.volume);
+    }
+}
---
+++ New Unity Project class03/Assets/Ship.cs
@@ -0,0 +1,34 @@
+﻿public class Ship
+{
+    public float speed;
+    //public float width { get; }
+
+    public float length { get; set; }
+
+    //public int password { set; }
+
+    //get (only read);set (only write <might get fail>)
+
+    private float width = 12.4f;
+    private float _width;
+
+    private float _height = 4.3f;
+    public float height { get => _height; set => _height = value; }
+
+    private float _volume;
+    public float volume
+    {
+        get
+        {
+            _volume = length * width * height;
+            return _volume;
+        }
+
+    }
+
+
+    public float Width { get { return _width; } }
+
+
+    
+}
